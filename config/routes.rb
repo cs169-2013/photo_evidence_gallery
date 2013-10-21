27adcf7735cc9ed98403f32abb0f94a040d347ac
saasbook <saasbook@saasbook.(none)>
2013-10-20 17:21:13 -0700

    new index view

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ PhotoApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'photos#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
