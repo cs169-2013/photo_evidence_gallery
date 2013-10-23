ce60d6659c04754e52e5442803b398aaa3d5ea5e
Edward Lai <edward.lai@berkeley.edu>
2013-10-22 23:35:47 -0700

    multiple upload loads all the images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ActiveRecord::Base
+	attr_accessor :edited
+	
 	def image_file=(input_data)
 		self.filename = input_data.original_filename
 		self.contentType = input_data.content_type.chomp
@@ -7,6 +9,7 @@ class Photo < ActiveRecord::Base
 
 	def create
 		Photo.create(params[:photo])
+		@edited = true
 	end
 
 end
