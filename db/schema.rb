0eb92007aeb4667ceef76ce7160d7597f147194e
Edward Lai <edward.lai@berkeley.edu>
2013-10-26 15:26:09 -0700

    Added some tests and fixed multiple upload to display messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131002063712) do
+ActiveRecord::Schema.define(version: 20131026220128) do
 
   create_table "photos", force: true do |t|
     t.string   "caption"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20131002063712) do
     t.binary   "binaryData"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "edited"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_Email", unique: true
 
 end
