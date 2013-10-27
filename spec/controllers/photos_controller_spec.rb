d55d1a386bed426a4e1a55465021611d15a25566
sage7will <william.huang@berkeley.edu>
2013-10-27 15:04:26 -0700

    rspec for create and update happy paths in photos_controller

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -39,6 +39,58 @@ describe PhotosController do
       response.should render_template :new
     end 
   end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates new photo" do
+        expect{
+          post :create, photo: FactoryGirl.attributes_for(:photo)
+        }.to change(Photo, :count).by(1)
+      end
+      
+      it "redirects to the new photo's page" do
+        post :create, photo: FactoryGirl.attributes_for(:photo)
+        response.should redirect_to photo_path(Photo.last)
+      end
+      
+      it "notifies that photo has been created" do
+        post :create, photo: FactoryGirl.attributes_for(:photo)
+        flash[:notice].should_not be_nil
+      end
+    end
+    
+    context "save failed" do
+    end
+  end
   
+  describe "PATCH/PUT #update" do    
+    before(:each) do
+      @photo1 = Photo.create!()
+      get :index
+    end
+    
+    context "with valid attributes" do
+      it "updates photo" do
+        expect{
+          put :update, id: @photo1.id, photo: FactoryGirl.attributes_for(:photo)
+        }.to_not change(Photo, :count)
+        @photo1.filename.should_not == Photo.last.filename
+        @photo1.binaryData.should_not == Photo.last.binaryData
+      end
+      
+      it "redirects to the updated photo's page" do
+        put :update, id: @photo1.id, photo: FactoryGirl.attributes_for(:photo)
+        response.should redirect_to photo_path(Photo.last)
+      end
+      
+      it "notifies that photo has been created" do
+        put :update, id: @photo1.id, photo: FactoryGirl.attributes_for(:photo)
+        flash[:notice].should_not be_nil
+      end
+    end
+    
+    context "update failed" do
+    end
+  end
   
 end
