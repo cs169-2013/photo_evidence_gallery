ce78702ce435e47eef1f690aa9ab7a6b719005d3
sage7will <william.huang@berkeley.edu>
2013-10-27 14:27:26 -0700

    rspec for index and new in photos_controller

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe PhotosController do
-
+  let(:factphoto) { FactoryGirl.create(:photo) }
+    
+  describe "GET #index" do
+    before(:each) do
+      photo1 = Photo.create!(:edited => true)
+      get :index
+    end
+    
+    it "populates an array of photos" do
+      assigns(:photos).should_not be_nil
+      assigns(:photos).length.should == 1
+      assigns(:photo_pack).length.should == 1
+    end
+    
+    it "leaves @binsize number of photos in each row" do
+      @counter = 0
+      while @counter < assigns(:bin_size)
+        @counter+=1
+        Photo.create!(:filename => "name_#{@counter}", :edited => true)
+      end
+      get :index
+      
+      assigns(:photos).length.should == assigns(:bin_size) + 1
+      assigns(:photo_pack).length.should == 2
+    end
+    
+    it "renders the :index view" do
+      get :index 
+      response.should render_template :index
+    end 
+  end
+  
+  describe "GET #new" do
+    it "renders the :new view" do
+      get :new 
+      response.should render_template :new
+    end 
+  end
+  
+  
 end
