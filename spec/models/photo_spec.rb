03f8948a553c4e7635b518d46f42eefd962398b6
sage7will <william.huang@berkeley.edu>
2013-10-27 00:56:58 -0700

    wrote rspecs for Photo model

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,10 +1,35 @@
 require 'spec_helper'
 
 describe Photo do
+  before(:all) do
+    @photo = Photo.new(caption: 'Default Caption', tags: 'Multiple tags', incidentName: 'Incident', operationalPeriod: 'Period', teamNumber: '12345', contentType: 'jpg', edited: true)
+  end
+  
+  subject { @photo }
+
+  it { should respond_to(:caption) }  
+  it { should respond_to(:tags) }  
+  it { should respond_to(:incidentName) }  
+  it { should respond_to(:operationalPeriod) }  
+  it { should respond_to(:teamNumber) }  
+  it { should respond_to(:contentType) }  
+  it { should respond_to(:filename) }  
+  it { should respond_to(:binaryData) }  
+  it { should respond_to(:created_at) }  
+  it { should respond_to(:updated_at) }  
+  it { should respond_to(:edited) }
+  
+  it { should be_valid }
+
+  
   it "sets its own fields with information" do
-    photo = FactoryGirl.create(:photo)
-    photo2 = FactoryGirl.create(:photo, :filename => "pic", :contentType => "png", :binaryData => "randomdata")
-    photo = photo2
-    assigns(photo.filename).should eq(photo2.filename)
+    mockphoto = mock_model(Photo)
+    mockphoto.stub(:original_filename).and_return("orig")
+    mockphoto.stub(:content_type).and_return(".png")
+    mockphoto.stub(:read).and_return("binaryData")
+    @photo.image_file = mockphoto
+    @photo.filename.should eq(mockphoto.original_filename)
+    @photo.contentType.should eq(mockphoto.content_type)
+    @photo.binaryData.should eq(mockphoto.read)
   end
 end
