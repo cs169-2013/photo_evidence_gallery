598cdc67cab300bfe4b5bd7f2ac4c4a974a7213a
sage7will <william.huang@berkeley.edu>
2013-10-27 00:27:51 -0700

    Setup a basic factory for photos

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Photo do
   it "sets its own fields with information" do
-    photo = Factory.create(:photo)
-    photo2 = Factory.create(:photo, :filename => "pic", :contentType => "png", :binaryData => "randomdata")
+    photo = FactoryGirl.create(:photo)
+    photo2 = FactoryGirl.create(:photo, :filename => "pic", :contentType => "png", :binaryData => "randomdata")
     photo = photo2
     assigns(photo.filename).should eq(photo2.filename)
   end
