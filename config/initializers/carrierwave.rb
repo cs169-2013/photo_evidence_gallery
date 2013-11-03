89beac74b4411a61e19c1305886fb911f6965b39
Chris <chris@Chris-Desktop.(none)>
2013-11-02 23:49:51 -0700

    Testing S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
   config.storage = :fog
+
   config.fog_credentials = {
-    :provider               => 'AWS',                       # required
-    :aws_access_key_id      => 'AKIAJEXL5HZ4SJD6NXEA',    # required
-    :aws_secret_access_key  => '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'     # required
+    :provider               => "AWS",
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
+    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
   }
-  config.fog_directory  = 'chrishsu2312testbucket'         # required
+  config.fog_directory = ENV["AWS_S3_BUCKET"]
+
 end
