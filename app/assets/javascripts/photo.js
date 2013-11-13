c8a7f9d2b89c27608a0f203c4c13602267c3e8fb
Kevin Casey <kacasey@berkeley.edu>
2013-11-12 18:00:23 -0800

    cleaned javascipt button for geolocating

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -33,19 +33,19 @@ $(document).ready(function () {
   $("#coorbutton").click(function(e) {
     var x=document.getElementById("Locator");
     function getLocation()
-    {
-      x.innerHTML="Locating....";
-    if (navigator.geolocation)
       {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      x.innerHTML="Locating....";
+      if (navigator.geolocation)
+        {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        }
+      else{x.innerHTML="Geolocation is not supported by this browser.";}
       }
-    else{x.innerHTML="Geolocation is not supported by this browser.";}
-    }
     function showPosition(position)
     {
       x.innerHTML="Success";
-      $('#photo_lat').val(position.coords.latitude);
-      $('#photo_lng').val(position.coords.longitude);
+      $("#photo_lat").val(position.coords.latitude);
+      $("#photo_lng").val(position.coords.longitude);
     }
     getLocation();
       
