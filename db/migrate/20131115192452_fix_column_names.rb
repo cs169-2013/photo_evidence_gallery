3966148c16eacc2ab38510ef57529367b7501315
Kevin Casey <kacasey@berkeley.edu>
2013-11-15 12:03:28 -0800

    removed binary data

--- db/migrate/20131115192452_fix_column_names.rb
+++ db/migrate/20131115192452_fix_column_names.rb
@@ -5,6 +5,6 @@ class FixColumnNames < ActiveRecord::Migration
   	rename_column :photos, :teamNumber, :team_number
   	rename_column :photos, :contentType, :content_type
   	rename_column :photos, :filename, :file_name
-  	remove_column :photos, :binaryData
+  	#remove_column :photos, :binaryData
   end
 end
