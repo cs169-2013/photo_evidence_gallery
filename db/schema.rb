2b5b584e616e787114a41c5853922725a87d16eb
timothyquach <timothyquach94@gmail.com>
2013-11-17 21:58:23 -0800

    added roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117015057) do
+ActiveRecord::Schema.define(version: 20131118054602) do
 
   create_table "photos", force: true do |t|
     t.string   "caption"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20131117015057) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
