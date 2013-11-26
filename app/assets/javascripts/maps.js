0337421dd744bc8211d7f7230706b1d405d49081
Kevin Casey <kacasey@berkeley.edu>
2013-11-26 14:31:09 -0800

    actually fixed the javascript index maps

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,9 +1,7 @@
 var map;
-var geocoder;
 
 function make_map(pins) //pins is a list of pins, a pin is the list [lat, lng, name]
 {
-	geocoder = new google.maps.Geocoder();
 	var mapOptions = {
 		center: new google.maps.LatLng(pins[0][0], pins[0][1]),
 		zoom: 8,
@@ -12,7 +10,7 @@ function make_map(pins) //pins is a list of pins, a pin is the list [lat, lng, n
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	var infowindow = new google.maps.InfoWindow();
-	if (pins != null)
+	if (pins != null) {
 		for (var i = 0; i < pins.length; i++){
 		  marker = new google.maps.Marker({
 			  position: new google.maps.LatLng(pins[i][0], pins[i][1]),
@@ -26,4 +24,5 @@ function make_map(pins) //pins is a list of pins, a pin is the list [lat, lng, n
 			  }
 			})(marker, i));
 		}
+	}
 }
