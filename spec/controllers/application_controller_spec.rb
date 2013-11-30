310106ec2aaecb065e40a603c1f02bb9cb25a5d9
sage7will <william.huang@berkeley.edu>
2013-11-29 16:38:58 -0800

    added application_controller spec

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ApplicationController do
+=begin
+  describe "#authenticate_admin!" do
+    it "errors if the user is invalid" do
+      stub(authenticate_user!).and_return(false)
+      authenticate_admin!
+    end
+    
+    it "errors if the current user is not an admin" do
+    end
+  end
+  describe "#authenticate_member!" do
+  end
+=end
+end
