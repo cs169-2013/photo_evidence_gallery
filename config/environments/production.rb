ea8ece03377afe2e6ca44909c0732400f5ce3057
Kevin Casey <kacasey@berkeley.edu>
2013-12-01 21:22:05 -0800

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,14 +4,14 @@ PhotoApp::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -25,7 +25,7 @@ PhotoApp::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.assets.debug = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  end
+end
