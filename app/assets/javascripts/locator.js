bff4b32ee8c739e2847b96c9fbabe4a763e983c2
Kevin Casey <kacasey@berkeley.edu>
2013-12-04 14:38:39 -0800

    added a time button

--- app/assets/javascripts/locator.js
+++ app/assets/javascripts/locator.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
   $("#coorbutton").click(function(e) {
-    var x=document.getElementById("Locator");
+    var x=$("#coorbutton");
     function getLocation()
       {
-      x.innerHTML="Locating....";
+      x.prop('value', "Locating....");
       if (navigator.geolocation)
         {
         navigator.geolocation.getCurrentPosition(showPosition);
         }
-      else{x.innerHTML="Geolocation is not supported by this browser.";}
+      else{x.prop('value', "Geolocation is not supported by this browser.");}
       }
     function showPosition(position)
     {
-      x.innerHTML="Success";
+      x.prop('value', "Success");
       $("#photo_lat").val(position.coords.latitude);
       $("#photo_lng").val(position.coords.longitude);
       $("#photos_lat").val(position.coords.latitude);
