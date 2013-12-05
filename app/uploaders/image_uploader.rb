8c9575ac0b949aa66ce69a04f35d4f7a58b632ed
sage7will <william.huang@berkeley.edu>
2013-12-04 17:09:37 -0800

    add timestamp to s3 saved filename

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,6 +21,15 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :shrink
   end
 
+  def filename
+    @name ||= "#{timestamp}-#{super}" if original_filename.present? and super.present?
+  end
+
+  def timestamp
+    var = :"@#{mounted_as}_timestamp"
+    model.instance_variable_get(var) or model.instance_variable_set(var, Time.now.to_i)
+  end
+
   def rotate
     if model.rotation.present?
       resize_to_limit(600, 600)
