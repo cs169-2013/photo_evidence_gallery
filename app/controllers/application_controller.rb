8f55b443873ce8b4e8b0a827285c0ad55bb246ed
Kevin Casey <kacasey@berkeley.edu>
2013-12-05 22:07:52 -0800

    made flickr view cool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-    protect_from_forgery with: :exception
-    require 'csv'
+  protect_from_forgery with: :exception
+  require 'csv'
 
-    def authenticate_admin!
-        redirect_to root_path, alert: "You do not have permission(admin) to access that" unless (authenticate_user! and current_user.role == "admin")
-    end
+  def authenticate_admin!
+    redirect_to root_path, alert: "You do not have permission(admin) to access that" unless (authenticate_user! and current_user.role == "admin")
+  end
 
-    def authenticate_member!
-        redirect_to root_path, alert: "You do not have permission(member) to access that" unless (authenticate_user! and (current_user.role == "admin" || current_user.role == "member"))
-    end
+  def authenticate_member!
+    redirect_to root_path, alert: "You do not have permission(member) to access that" unless (authenticate_user! and (current_user.role == "admin" || current_user.role == "member"))
+  end
 
 
 end
