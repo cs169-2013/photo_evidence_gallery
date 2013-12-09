cc59ca0b493497f6916430b3262d91d452300ab5
sage7will <william.huang@berkeley.edu>
2013-12-09 12:58:12 -0800

    deleted csv pending tests

--- spec/controllers/csv_controller_spec.rb
+++ spec/controllers/csv_controller_spec.rb
@@ -18,32 +18,28 @@ describe CsvController do
       it "needs this blank test" do
         #pending "so i wrote a dead test to make the other tests pass...this is pretty awk"
       end
-      it "parses the csv file" do
-        pending
-        #assigns(:csv).should_not be_nil
-      end
       it "redirects to csv page" do
         response.should redirect_to csv_index_path
       end
     end
+=begin
     context "when importing an invalidly formatted file" do
       before(:each) do
         post :import, :csv_file => Rack::Test::UploadedFile.new(filepath+ "user_invalid_test.csv", "application/csv")
       end
       it "populates the flash with updates" do
         pending
-=begin
         flash.should_not be_empty
         flash.key?('email').should be_false #column header
         flash.key?('chiller@berkeley.edu').should be_true #first entry
         flash.key?('whuang@berkeley.edu').should be_false # improperly formatted
         flash.key?('andyl@berkeley.edu').should be_true #last entry
-=end
       end
     end
     context "when importing an incorrect filetype" do
       pending
     end
+=end
   end
   
 	describe "tests that want to stub things" do
