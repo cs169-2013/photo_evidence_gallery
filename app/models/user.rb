c587719acd2bb02b0bf3d64c5b03f2a1b95d03e7
Kevin Casey <kacasey@berkeley.edu>
2013-12-09 18:14:45 -0800

    flickr link

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   serialize :info, Hash
+  attr_accessor :flick
 end
